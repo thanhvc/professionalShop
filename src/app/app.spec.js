630f785e6fb554d136878156f29b540b43246bef
Aitor Carrera <aitor.carrera@edosoft.es>
2014-04-10 23:31:43 +0100

    first commit
    Initial commit

---
+++ src/app/app.spec.js
@@ -0,0 +1,23 @@
+/*
+ describe( 'AppCtrl', function() {
+ describe( 'isCurrentUrl', function() {
+ var AppCtrl, $location, $scope, $httpBack;
+
+ beforeEach( module( 'ngMo' ) );
+
+ beforeEach( inject( function( $controller, _$location_, $rootScope, $httpBackend  ) {
+ $location = _$location_;
+ $scope = $rootScope.$new();
+ $httpBack = $httpBackend;
+ jasmine.log($httpBack);
+ AppCtrl = $controller( 'AppCtrl', { $location: $location, $scope: $scope });
+ }));
+
+
+ it( 'should pass a dummy test', inject( function() {
+ $httpBack.expectGET('i18n/lang-es.json').respond([]);
+ expect( AppCtrl ).toBeTruthy();
+ }));
+ });
+ });
+ */
