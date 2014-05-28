d158871034ee546662ca6afffaa9aca882351747
robertogonzalezsuarez <roberto.gonzalez@edosoftfactory.com>
2014-05-28 17:31:39 +0100

    patterns

--- src/app/my_patterns/my_patterns.js
+++ src/app/my_patterns/my_patterns.js
@@ -21,32 +21,41 @@ angular.module('ngMo.my_patterns', [
             }
         });
     })
-    .controller('PatternsCtrl', function HomeController($scope, $templateCache, $rootScope, ActiveTabService, ActualDateService) {
+    .controller('PatternsCtrl', function HomeController($scope, $templateCache, $rootScope, ActiveTabService, ActualDateService, PacksService) {
        //tabs
-        $scope.tabs = [
+        $scope.tabs =  [
             {
                 title: 'Acciones',
                 active: ActiveTabService.activeTab() === 0,
-                value: 0
-                //url: 'home/tables_packs/stock_table.tpl.html'
+                value: 0,
+                americaContent: PacksService.obtainPacks('america'),
+                asiaContent: PacksService.obtainPacks('asia'),
+                europeContent: PacksService.obtainPacks('europe'),
+                url: 'home/tables_packs/stock_table.tpl.html'
             },
             {
                 title: 'Pares',
                 active: ActiveTabService.activeTab() === 1,
-                value: 1
-               // url: 'home/tables_packs/pairs_table.tpl.html'
+                value: 1,
+                americaContent: PacksService.obtainPacks('americaPairs'),
+                asiaContent: PacksService.obtainPacks('asiaPairs'),
+                europeContent: PacksService.obtainPacks('europePairs'),
+                url: 'home/tables_packs/pairs_table.tpl.html'
             },
             {
                 title: 'Indices',
                 active: ActiveTabService.activeTab() === 2,
-                value: 2
-               // url: 'home/tables_packs/indices_table.tpl.html'
+                value: 2,
+                indicesContent: PacksService.obtainPacks('indices'),
+                pairsIndicesContent: PacksService.obtainPacks('pairs_indices'),
+                url: 'home/tables_packs/indices_table.tpl.html'
             },
             {
                 title: 'Futuros',
                 active: ActiveTabService.activeTab() === 3,
-                value: 3
-                //url: 'home/tables_packs/futures_table.tpl.html'
+                value: 3,
+                futuresContent: PacksService.obtainPacks('futures'),
+                url: 'home/tables_packs/futures_table.tpl.html'
             }
         ];
         selectedTab = ActiveTabService.activeTab();
