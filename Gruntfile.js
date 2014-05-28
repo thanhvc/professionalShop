b48c84fd0a4e7c8c2b2fc2fa6283e600511773a2
Aitor Carrera <aitor.carrera@edosoft.es>
2014-05-28 15:24:00 +0100

    e2e test configuration and example

--- Gruntfile.js
+++ Gruntfile.js
@@ -588,7 +588,7 @@ module.exports = function ( grunt ) {
     grunt.registerTask( 'build', [
         'clean', 'html2js', 'jshint', 'coffeelint', 'coffee', 'less:build',
         'concat:build_css', 'copy:build_app_assets', 'copy:build_vendor_assets',
-        'copy:build_appjs', 'copy:build_vendorjs', 'index:build', 'karmaconfig', 'e2e',
+        'copy:build_appjs', 'copy:build_vendorjs', 'index:build', 'karmaconfig',
         'karma:continuous'
     ]);
 
