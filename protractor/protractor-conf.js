4f1bf43a527c4094544a082753c253b768d0270d
laiaperez <laia.perez@edosoftfactory.com>
2014-06-09 11:17:26 +0100

    Signup tests done by Page Pattern

--- protractor/protractor-conf.js
+++ protractor/protractor-conf.js
@@ -4,10 +4,10 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     suites: {
-        homepage: '../src/app/home/*protractor.js',
-        layout: '../src/app/*protractor.js',
-        signup:' ../src/app/sign_up/*protractor.js',
-        cart:' ../src/app/home/catalog/*protractor.js',
+        //homepage: '../src/app/home/*protractor.js',
+        //layout: '../src/app/*protractor.js',
+        signup:' ../src/app/sign_up/*protractor.js'
+        //cart:' ../src/app/home/catalog/*protractor.js'
         //menu:' ../src/app/layout_templates/*protractor.js'
 
 
--- src/app/layout_templates/menu.protractor.js
+++ src/app/layout_templates/menu.protractor.js
@@ -11,20 +11,33 @@ describe('The menu is ok', function() {
 
         var menu = element(by.id("market-observatory-nav"));
         menu.click();
-        expect(menu.getCssValue('background-color','orange'));
+        expect(menu.getCssValue('background-color')).toBe('rgba(255, 130, 0, 1)');
 
 
     });
 
     it('should change the text when changing the tab', function () {
 
-        var link = element.all(by.css('li'));
-
-
-       // link.get(6).click();
-
-        //var text = element(by.css('text-block-title')).getText();
-        //expect(text).toBe('Organizamos');
+       // var link = element.all(by.css('li'));
+       var menu = element(by.id('services-nav'));
+       menu.click();
+       var submenu = element(by.id('summary-nav'));
+       submenu.click();
+       var text = element(by.css('text-block-title'));
+       //text = text.getText();
+       //expect(text).toBe('Resumen');
+       //console.log(text.getText());
 
     });
+
+    it('should get orange when mouse over',function(){
+
+        var menu = element(by.id("market-observatory-nav"));
+        menu.click();
+        browser.actions().mouseMove($('#organization-nav')).perform();
+        var submenu = element(by.id('organization-nav'));
+       // expect(submenu.getCssValue('background-color')).toBe('rgba(255, 130, 0, 1)');
+        expect(submenu.getCssValue('background-color')).toBe('rgba(0, 0, 0, 0)');
+    });
+
 });
