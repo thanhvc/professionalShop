658fa4b419be6ec61f192448ae48f74ffcec3876
Aitor Carrera <aitor.carrera@edosoft.es>
2014-06-10 13:35:29 +0100

    remove phantom

--- protractor/protractor-conf.js
+++ protractor/protractor-conf.js
@@ -12,20 +12,28 @@ exports.config = {
         patterns:' ../src/app/my_patterns/*protractor.js'
 
     },
-    capabilities: {
-        'browserName': 'phantomjs',
+    multiCapabilities: [
+        /* {
+         'browserName': 'phantomjs',
 
-        /*
+         /*
          * Can be used to specify the phantomjs binary path.
          * This can generally be ommitted if you installed phantomjs globally.
-         */
-        'phantomjs.binary.path': './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs',
 
-        /*
+         'phantomjs.binary.path': './node_modules/karma-phantomjs-launcher/node_modules/phantomjs/bin/phantomjs',
+
+         /*
          * Command line arugments to pass to phantomjs.
          * Can be ommitted if no arguments need to be passed.
          * Acceptable cli arugments: https://github.com/ariya/phantomjs/wiki/API-Reference#wiki-command-line-options
-         */
-        'phantomjs.cli.args': [ "--ignore-ssl-errors=true", ]
-    }
+
+         'phantomjs.cli.args': [ "--ignore-ssl-errors=true", ]
+         },*/
+        {
+            browserName: 'chrome'
+        }/*,
+         {
+         browserName:'firefox'
+         }*/
+    ]
 }
