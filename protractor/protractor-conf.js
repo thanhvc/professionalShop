f728a6f0dbecf0143ddf0fff5eadeb354f9cc7be
laiaperez <laia.perez@edosoftfactory.com>
2014-06-10 12:45:41 +0100

    Testing private part
    Testing Bolsa
      -Check if table is shown
      -Check if table name is correct
      -Check if 'Show more' button works
      -Check if date is the current date
    
    Testing Commodities
      -Check if table is shown
      -Check if table name is correct
      -Check if date is the current date
    
    Testing S&P
      -Check if table is shown
      -Check if table name is correct
       -Check if date is the current date

--- protractor/protractor-conf.js
+++ protractor/protractor-conf.js
@@ -4,11 +4,11 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     suites: {
-        //homepage: '../src/app/home/*protractor.js',
-        //layout: '../src/app/*protractor.js',
-        //signup:' ../src/app/sign_up/*protractor.js',
-       // cart:' ../src/app/home/catalog/*protractor.js',
-        //menu:' ../src/app/layout_templates/*protractor.js',
+        homepage: '../src/app/home/*protractor.js',
+        layout: '../src/app/*protractor.js',
+        signup:' ../src/app/sign_up/*protractor.js',
+        cart:' ../src/app/home/catalog/*protractor.js',
+        menu:' ../src/app/layout_templates/*protractor.js',
         patterns:' ../src/app/my_patterns/*protractor.js'
     }
 }
