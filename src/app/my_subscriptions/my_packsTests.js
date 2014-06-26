e6c7cf8b2b7a78e7b9e40d37d5dec54347f94014
laiaperez <laia.perez@edosoftfactory.com>
2014-06-26 14:25:17 +0100

    My packs and Volatility tests done.
    -Check if tables appear
    -Check if numbers of lines are correct
    -Check if data is loaded

--- src/app/my_subscriptions/my_packsTests.js
+++ src/app/my_subscriptions/my_packsTests.js
@@ -10,20 +10,21 @@
 */
 /**
  * Created by laia on 25/06/14.
- *//*
+ */
 
-var App = function(){
+var Packs = function(){
 
     this.open = function(){
-        browser.get('http://localhost:63342/mo-shopclient/build/index.html#/volatility');
+        browser.get('http://localhost:63342/mo-shopclient/build/index.html#');
         browser.ignoreSynchronization = true;
     };
 
     this.login = function(){
         element(by.css(".no-logged-box")).click();
-        element(by.css(".float-right")).sendKeys('laiarios@gmail.com');
-        element.all(by.css(".float-right")).get(1).sendKeys('prueba1');
+        element(by.css(".float-right")).sendKeys('laia@gmail.com');
+        element.all(by.css(".float-right")).get(1).sendKeys('prueba12');
         element(by.css(".mo-button")).click();
+        browser.get('http://localhost:63342/mo-shopclient/build/index.html#/my-subscriptions/my-packs')
     };
 
     this.checkNumberOfLines = function(){
@@ -38,19 +39,24 @@ var App = function(){
 
 describe('My packs page ', function() {
 
-    var a = new App();
+    var a = new Packs();
     it('should appear when volatility link is clicked', function () {
         a.open();
-        // a.login();
+        a.login();
         expect(browser.isElementPresent(by.css('.private-black-title'))).toBe(true);
     });
 
-    it('should have 32 lines', function(){
+    it('should have 46 lines', function(){
 
         a.checkNumberOfLines();
-        expect(a.n).toBe(32);
+        expect(a.n).toBe(46);
     });
 
+    it('should load the data', function(){
+
+        a.checkDataLoaded();
+        expect(a.row.getText()).toBe('Australia + Nueva Zelanda Pack I');
+    })
 
 
-});*/
+});
