2d671c589e864db06ae631771e44de2b38d8cb59
laiaperez <laia.perez@edosoftfactory.com>
2014-07-25 12:08:50 +0100

    Corrections done

--- protractor/protractor-conf.js
+++ protractor/protractor-conf.js
@@ -43,12 +43,12 @@ exports.config = {
          browserName:'firefox'
          }*/
     ],
-    onPrepare: function () {
+    /*onPrepare: function () {
         // The require statement must be down here, since jasmine-reporters
         // needs jasmine to be in the global and protractor does not guarantee
         // this until inside the onPrepare function.
         require('jasmine-reporters');
         jasmine.getEnv().addReporter(
             new jasmine.JUnitXmlReporter('e2e-test-results/', true, true));
-    }
+    }*/
 }
