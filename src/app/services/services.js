cd3096247f1a8c804e8c8e6ceeb79752a7b5292c
lauraaleman <lauraaleman@edosoftfactory.com>
2014-07-31 13:34:14 +0100

    Added portfolio views

--- src/app/services/services.js
+++ src/app/services/services.js
@@ -154,7 +154,7 @@ angular.module('ngMo.services', [
          url: '/correlation',
          views: {
          "subPage": {
-         templateUrl: 'services/detailed_description/correlation.tpl.html'
+         templateUrl: 'services/detailed_description/portfolio.tpl.html'
          }
          }
          })
