39a030a4bb6f1f06dc980481234a9eda73ab1751
laiaperez <laia.perez@edosoftfactory.com>
2014-08-12 10:42:19 +0100

    Reestructure my account

--- protractor/protractor-conf.js
+++ protractor/protractor-conf.js
@@ -4,14 +4,15 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     suites: {
-        homepage: '../src/app/home/*protractor.js',
+       /* homepage: '../src/app/home/*protractor.js',
         layout: '../src/app/*protractor.js',
         signup:' ../src/app/sign_up/*protractor.js',
         cart:' ../src/app/home/catalog/*protractor.js',
         menu:' ../src/app/layout_templates/*protractor.js',
         patterns:' ../src/app/my_patterns/*protractor.js',
         volatility: ' ../src/app/tools/volatility/*protractor.js',
-        packs: '../src/app/my_subscriptions/*protractor.js'
+        packs: '../src/app/my_subscriptions/*protractor.js'*/
+        profile: '../src/app/my_profile/*protractor.js'
 
     },
     multiCapabilities: [
@@ -33,9 +34,7 @@ exports.config = {
     //        'phantomjs.cli.args': [ "--ignore-ssl-errors=true", "--web-security=false"]
         //},
 
-
-
-            browserName: 'chrome'
+            browserName: 'firefox'
         }
        // },
         /*
@@ -43,6 +42,7 @@ exports.config = {
          browserName:'firefox'
          }*/
     ],
+
     /*onPrepare: function () {
         // The require statement must be down here, since jasmine-reporters
         // needs jasmine to be in the global and protractor does not guarantee
