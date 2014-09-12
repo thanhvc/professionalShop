e1ee5e9116f6079027d9af7cedea285f7bae88d0
laiaFighter <laia.fighter@gmail.com>
2014-09-12 13:57:04 +0100

    Payment controller tests

--- src/app/my_profile/my_profile.spec.js
+++ src/app/my_profile/my_profile.spec.js
@@ -1,35 +1,39 @@
-describe('My profile service', function() {
-    var $scope, ctrl, $http, $state,service;
-    beforeEach(angular.mock.module("ngMo"));
-    beforeEach(angular.mock.module("ngMo.my_profile"));
+describe('My profile controller', function()  { 
+    var $scope, ctrl, $http, $state; 
+    beforeEach(angular.mock.module("ngMo")); 
+    beforeEach(angular.mock.module("ngMo.my_profile")); 
+ 
+ 
+    beforeEach(inject(function ($controller, $rootScope, _$http_, _$httpBackend_, _$state_)  { 
+     
+        //_$httpBackend_.when('GET', $rootScope.urlService + '/homepacks').respond(200); 
+        ctrl = $controller; 
+        $scope = $rootScope.$new(); 
+        $http = _$httpBackend_; 
+        $state = _$state_; 
+        $state.$current = {data :  {  pageTitle : 'title', subPage: 1}};
+        $state = {data :  {  pageTitle : 'title'}};
+        $controller('ProfileCtrl',  {'$rootScope': $rootScope, '$http': _$http_, '$scope': $scope });
+     
+     })); 
+ 
+    it("should be able to restart user", function ()  { 
+        $scope.restartUser(); 
+        expect($scope.restartUser).toNotBe(undefined); 
+     }); 
+ 
+    it("should be able to save user", function ()  { 
+        $scope.saveUser(); 
+        expect($scope.saveUser).toNotBe(undefined); 
+     }); 
+ 
+    it("should be able to save users Password", function ()  { 
+        $scope.savePassword(); 
+        expect($scope.savePassword).toNotBe(undefined); 
+     }); 
+ 
+    it("should success when state start changes", function ()  { 
+        $scope.$broadcast('$stateChangeStart');
+     }); 
 
-
-    beforeEach(inject(function (ProfileService, $rootScope, _$http_, _$httpBackend_, _$state_) {
-
-        service = ProfileService;
-        $scope = $rootScope.$new();
-        $http = _$httpBackend_;
-        $state = _$state_;
-        $state.$current = {
-            data :{ subPage : 1}
-        };
-
-    }));
-
-    it("should be able to load user", function () {
-        service.loadUser();
-        expect(service.loadUser).toNotBe(undefined);
-    });
-
-    it("should be able to edit user", function () {
-        service.editUser();
-        expect(service.editUser).toNotBe(undefined);
-    });
-
-    it("should be able to edit password", function () {
-        service.editPassword('password','callback');
-        expect(service.editPassword).toNotBe(undefined);
-    });
-
-
-});
+ });
