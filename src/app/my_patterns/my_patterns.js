e4c1b25a74359dea257fbb9c48df44401cf14bf4
robertogonzalezsuarez <roberto.gonzalez@edosoftfactory.com>
2014-09-17 18:15:51 +0100

    fix calendar

--- src/app/my_patterns/my_patterns.js
+++ src/app/my_patterns/my_patterns.js
@@ -226,7 +226,7 @@ angular.module('ngMo.my_patterns', [
             angular.forEach(filters, function(value, key) {
                 if (key !== "month" && key !== "selectMonth" && key !== "active_tab" && key !== "index_type" && key !== "tab_type" && key !=="order") { //these filters always are provided
                     //in case of order, in the calendar the order input doesnt affect to patterns to load
-                    if (value !== "") {
+                    if ((value !== "") && (value !== null)) {
                         console.log(key+" "+value);
                         temp = true;
                     }
