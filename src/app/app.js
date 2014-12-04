088dea2fb327fb44f38500a6ea5b4a37f871a96f
yeraycabrera <yeray.cabrera@edofostfactory.com>
2014-12-04 12:58:53 +0000

    Spike #3090

--- src/app/app.js
+++ src/app/app.js
@@ -943,7 +943,7 @@ angular.module('ngMo', [
 
 
     })
-    .controller('AppCtrl', function AppCtrl($scope, $rootScope, ActualDateService, $modal, IsLogged, AnchorLinkService,$http,$translate,$translatePartialLoader,tmhDynamicLocale) {
+    .controller('AppCtrl', function AppCtrl($scope, $rootScope, ActualDateService, $modal, IsLogged, AnchorLinkService,$http,$translate,$translatePartialLoader,tmhDynamicLocale,$location, $timeout) {
         //Set when is logged
         IsLogged.checkLogged();
 
@@ -971,6 +971,10 @@ angular.module('ngMo', [
                 $scope.inVolatilityView = true;
             }
 
+            if ($location.path() == "/home" && toState.name == "catalog"){
+                $scope.scrollPos = window.scrollY;
+                console.log($scope.scrollPos );
+            }
         });
         $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
             if (angular.isDefined(toState.data.pageTitle)) {$scope.pageTitle = toState.data.pageTitle + ' | Market Observatory';}
@@ -984,6 +988,15 @@ angular.module('ngMo', [
             $scope.$watch('actualSubmenu', function(){});
             $scope.$watch('selectSubmenu', function(){});
             AnchorLinkService.scrollTo('top');
+
+            if (toState.name == ("home") && fromState.name == "catalog"){
+                $timeout(function() { // wait for DOM, then restore scroll position
+                    window.scroll(0,  $scope.scrollPos ? $scope.scrollPos : 0);
+                }, 1000);
+                console.log($scope.scrollPos );
+            }
+
+
         });
         var data = ActualDateService.actualDate(function (data) {
             $scope.actualDate = data.actualDate;
@@ -1038,6 +1051,7 @@ angular.module('ngMo', [
             tmhDynamicLocale.set(lang);
         };
 
+        $scope.scrollPos = 0; // scroll position of each view
     })
 
     .directive("scroll", function ($window) {
