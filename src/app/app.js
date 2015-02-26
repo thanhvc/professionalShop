32c5abf375afc8d066387e78b6e29f7949d67cd9
David Verdú <david.verdu@edosoftfactory.com>
2015-02-26 13:14:23 +0000

    Update FAQ page. Now is a main page divided into sections

--- src/app/app.js
+++ src/app/app.js
@@ -110,6 +110,7 @@ angular.module('ngMo', [
     .run(function run($rootScope,$translate,$translateCookieStorage) {
        $rootScope.urlService = 'http://api.mo.devel.edosoftfactory.com';
        //$rootScope.urlService = 'http://localhost:19000';
+       //$rootScope.urlService = 'http://localhost:9000';
         $rootScope.$on('$translatePartialLoaderStructureChanged', function () {
             $translate.use($translateCookieStorage.get('lang'));
             $translate.refresh();
@@ -1171,6 +1172,30 @@ angular.module('ngMo', [
             $translate.refresh();
             tmhDynamicLocale.set(lang);
         };
+       
+        //maps each page to FAQ section. No mapping means no FAQ 
+        $scope.faqMap = {
+            "/home"            : "home",
+            "/stocks"          : "service_application",
+            "/funds"           : "service_application",
+            "/etf_cfd"         : "service_application",
+            "/futures"         : "service_application",
+            "/pairs"           : "service_application",
+            "/advanced"        : "service_application",
+            "/diversification" : "service_application"
+        };
+
+        $scope.goToFAQ = function() {
+             var fromPath = $location.path().split("#")[0];
+             var faqSection = $scope.faqMap[fromPath];
+             if (faqSection === null || faqSection === undefined) {
+                 $location.path("/no_faq"); //go to no_faq page when no FAQ section defined
+             } else {
+                 $location.path("/faq");
+                 $location.hash(faqSection);
+             }
+        };
+
 
         $scope.scrollPos = 0; // scroll position of each view
     })
