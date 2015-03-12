beafa148dd316fd0f902a973b633aa010b29ef1d
robertogonzalezsuarez <roberto.gonzalez@edosoftfactory.com>
2015-03-12 16:07:29 +0000

    change sub menu with back button.

--- src/app/app.js
+++ src/app/app.js
@@ -1368,6 +1368,9 @@ angular.module('ngMo', [
                     $scope.actualSubmenu = '';
                     $scope.actualItemSubmenu = '';
                 };
+                $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+                    $scope.actualItemSubmenu = '';
+                });
             },
             link: function($scope) {
                 $scope.$watch('actualSubmenu', function(){});
